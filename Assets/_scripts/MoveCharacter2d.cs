4674e1ea24255a106ec7c2344a19333ce74f42b9
blkfinch <blkfinch@gmail.com>
2020-08-15 20:55:08 -0600

    working player movement

---
+++ Assets/_scripts/MoveCharacter2d.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MoveCharacter2d : MonoBehaviour
+{
+    #region Variables and Components
+    public float speed = 1.5f;
+    public float targetStopRange = 1f;
+
+    private Vector3 moveTarget;
+    private float distanceToTarget;
+    private bool isWalking = false;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    #endregion
+
+    void Awake()
+    {
+        //init moveTarget to prevent drift on awake
+        //
+        moveTarget = transform.position;
+
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    void Update()
+    {
+        if (distanceToTarget > targetStopRange)
+        {
+
+            ProcessMovement();
+            if (!isWalking) { PlayWalkingAnimation(true); }
+        }
+        else
+        {
+            if (isWalking) { PlayWalkingAnimation(false); }
+        }
+    }
+
+    public void SetMoveTarget(Vector3 target)
+    {
+        moveTarget = target;
+
+
+
+        if (moveTarget.x > transform.position.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+
+
+
+        ProcessMovement();
+    }
+
+    public void PlayTillAnimation()
+    {
+        animator.SetTrigger("Till");
+    }
+
+    private void PlayWalkingAnimation(bool _walking)
+    {
+        animator.SetBool("isWalking", _walking);
+        isWalking = _walking;
+    }
+
+    private void ProcessMovement()
+    {
+        moveTarget.z = transform.position.z;
+        moveTarget.y = transform.position.y;
+
+        transform.position = Vector3.MoveTowards(transform.position, moveTarget, speed * Time.deltaTime);
+
+        distanceToTarget = Vector3.Distance(moveTarget, transform.position);
+    }
+}
---
+++ Assets/_scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MoveCharacter2d))]
+public class PlayerInput : MonoBehaviour
+{
+    private MoveCharacter2d moveCharacter;
+    private Vector3 mouseTarget;
+
+    void Awake()
+    {
+        moveCharacter = this.GetComponent<MoveCharacter2d>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+             mouseTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+             moveCharacter.SetMoveTarget(mouseTarget);
+        }
+        else if(Input.GetMouseButtonDown(1))
+        {
+            moveCharacter.PlayTillAnimation();
+        }
+    }
+}
